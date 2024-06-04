da24cac60b4ccc3976ea449955bd7ab8934a7914
Jiang Le <1163280771@qq.com>
2024-06-04 20:23:43 +0800

    '初版'

---
+++ src/components/Error.vue
@@ -0,0 +1,20 @@
+<!--
+ * 文件描述: 报错提示
+ -->
+<template>
+  <div class="error">
+    <div class="main"></div>
+  </div>
+</template>
+<style>
+.error {
+  height: 500px;
+  width: 1225px;
+  margin: 0 auto;
+}
+.error .main {
+  margin: 0 362.5px;
+  height: 500px;
+  background: url(../assets/imgs/error.png) no-repeat;
+}
+</style>
